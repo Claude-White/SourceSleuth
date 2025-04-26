823ec22bdb61c9236259ab85999160100a2384db
Claude White <chitin.skit.0t@icloud.com>
2025-04-26 14:46:29 -0400

    Updated some styles and added icon

--- extension/src/popup.tsx
+++ extension/src/popup.tsx
@@ -1,14 +1,18 @@
-import "./styles.css"
+import "./styles.css";
 
-import React from "react"
+import React from "react";
+
+import myIcon from "~assets/logo.svg";
 
 function IndexPopup() {
-  return (
-    <div className="w-[400px] h-[500px] p-4">
-      <h1>Gemini AI Assistant</h1>
-      <button className="btn">Button</button>
-    </div>
-  )
+    return (
+        <div className="p-4 h-72 w-96">
+            <div className="flex items-end gap-1">
+                <img src={myIcon} className="w-8" alt="My Icon" />
+                <h1 className="text-xl font-bold">Source Sleuth</h1>
+            </div>
+        </div>
+    );
 }
 
-export default IndexPopup
+export default IndexPopup;
